486f3ce4cf5f5608b9301308de7a3a97a529d04a
angelas606 <angelnyrt607@gmail.com>
2025-10-18 12:22:20 +0700

    contoh

---
+++ contoh/Calculator.java
@@ -0,0 +1,10 @@
+package contoh;
+
+public class Calculator {
+
+    //Method untuk menjumlahkan bilangan bulat
+    public int add(int a, int b) {
+        //Mengembalikann hasil penjumlahan a dan b
+        return a + b;
+    }
+}
